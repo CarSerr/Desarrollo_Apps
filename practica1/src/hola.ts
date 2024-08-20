9e371ade164abfbaa5b9c7dc9190f5c05b7acea6
CarSerr <matachangos777@gmail.com>
2024-08-20 07:39:52 -0600

    Primer commit

---
+++ practica1/src/hola.ts
@@ -0,0 +1,6 @@
+let mensaje : string = 'Hola TypeScript';
+console.log(mensaje);
+
+let numero : number = 10;
+numero = 3.1416;
+console.log(numero);
---
+++ practica2/src/index.ts
@@ -0,0 +1,8 @@
+let miNumero : number = 10;
+
+miNumero = 10 + 10;
+miNumero = 2.5;
+console.log(miNumero);
+
+let booleano : boolean = true;
+booleano = false;
