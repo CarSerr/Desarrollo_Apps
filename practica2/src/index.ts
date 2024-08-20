4fd64e82020e9eced4326b687cffa551f425740f
CarSerr <matachangos777@gmail.com>
2024-08-20 08:40:56 -0600

    Segundo commit

--- practica2/src/index.ts
+++ practica2/src/index.ts
@@ -6,3 +6,24 @@ console.log(miNumero);
 
 let booleano : boolean = true;
 booleano = false;
+
+let encendido : boolean;
+//console.log(encendido);
+encendido = true;
+
+console.log('Encendido es:',encendido);
+console.log('Encendido es:',{encendido});
+
+encendido = true && false;
+encendido = true && true;
+encendido = true && true;
+encendido = false && true;
+encendido = false && false;
+
+console.log('Resultado:',encendido);
+
+let normal : any = 'hola';
+normal = 10;
+normal = true;
+normal = 11.25;
+console.log('Normal:',normal);
