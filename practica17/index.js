1ace018760db0d581430455d591a76477ce34663
CarSerr <matachangos777@gmail.com>
2024-10-19 12:07:17 -0600

    practica 17

--- practica17/index.js
+++ practica17/index.js
@@ -45,3 +45,47 @@ const obtieneNumero = () => {
 
 let result = _.times(5,obtieneNumero);
 console.log(result);
+
+// Reto 1
+// Crear un arreglo con 100 números aleatorios y mostrar los números pares
+// y los números impares en arreglos diferentes
+
+let numeros = _.times(100, () => _.random(1, 100));
+
+let pares = _.filter(numeros, num => num % 2 === 0);
+let impares = _.filter(numeros, num => num % 2 !== 0);
+
+console.log("Números pares: ", pares);
+console.log("Números impares: ", impares);
+
+//Reto 2
+// Crear un arreglo con 100 números aleatorios y mostrar los números primos
+// y los números no primos en arreglos diferentes
+
+const esPrimo = (num) => {
+    if (num <= 1) return false;
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+        if (num % i === 0) return false;
+    }
+    return true;
+};
+
+let numerosPrimos = _.times(100, () => _.random(1, 100));
+
+let primos = _.filter(numerosPrimos, esPrimo);
+let noPrimos = _.reject(numerosPrimos, esPrimo);
+
+console.log("Números primos: ", primos);
+console.log("Números no primos: ", noPrimos);
+
+//Reto 3
+// Crear un arreglo de nombres y mostrar los nombres que empiecen con la letra 'A'  
+// y los nombres que no empiecen con la letra 'A' en arreglos diferentes
+
+let nombres = ['Ana', 'Juan', 'Andrés', 'Luis', 'Alberto', 'Carla', 'Armando', 'Daniel'];
+
+let nombresConA = _.filter(nombres, nombre => _.startsWith(nombre, 'A'));
+let nombresSinA = _.reject(nombres, nombre => _.startsWith(nombre, 'A'));
+
+console.log("Nombres que empiezan con 'A': ", nombresConA);
+console.log("Nombres que no empiezan con 'A': ", nombresSinA);
